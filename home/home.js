5ee4cacab5fecf6341291a48ae0d1ed733081052
ManuCaporaso <manulpda@gmail.com>
2023-10-19 17:38:08 -0300

    Cambios Finales

--- home/home.js
+++ home/home.js
@@ -1,3 +1,5 @@
+//CARROUSEL DE LIBROS
+
 const carousel = document.querySelector(".carousel");
 
 carousel.addEventListener("scroll", () => {
@@ -12,3 +14,46 @@ carousel.addEventListener("scroll", () => {
         book.classList.toggle("active", bookIndex === index);
     });
 });
+
+
+//CARROUSELL DE NOVEDADES
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Cambiar las imágenes automáticamente cada 5 segundos
+setInterval(() => {
+    plusSlides(1);
+}, 5000);
