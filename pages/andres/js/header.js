820870542ee13f8ea42d42bcf597f4ecd93e31e1
arnaldochaloupka <aachaloupka@gmail.com>
2023-11-26 18:42:16 -0300

    Base de Sugerencia de compra appy

---
+++ pages/andres/js/header.js
@@ -0,0 +1,7 @@
+document.getElementById("header").innerHTML = `
+<nav class="menu">
+<a href="../tabla_productos.html">Sugerencias Realizadas</a>
+<a href="ingresar_producto.html">Registrar Sugerencia</a>
+<a href="../andres.html">Volver Página Principal</a>
+</nav>
+`;
