a0ca3bbecef109a4a9254e6a6ccdd97985ec1887
arnaldochaloupka <aachaloupka@gmail.com>
2023-11-30 10:39:53 -0300

    nueva sugerencia en  barra menú

--- pages/andres/js/header.js
+++ pages/andres/js/header.js
@@ -1,5 +1,6 @@
 document.getElementById("header").innerHTML = `
 <nav class="menu">
+<a class="nuevo" href="ingresar_producto.html">Nueva Sugerencia</a>
 <a href="../index.html">Sugerencias Realizadas</a>
 <a href="ingresar_producto.html">Registrar Sugerencia</a>
 <a href="../andres.html">Volver</a>
