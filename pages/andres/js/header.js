d4cf42c7fd1bff4a4216806330d8e13efe99c5ec
arnaldochaloupka <aachaloupka@gmail.com>
2023-11-29 22:15:11 -0300

    Correcion de tabla responsive

--- pages/andres/js/header.js
+++ pages/andres/js/header.js
@@ -2,6 +2,6 @@ document.getElementById("header").innerHTML = `
 <nav class="menu">
 <a href="../index.html">Sugerencias Realizadas</a>
 <a href="ingresar_producto.html">Registrar Sugerencia</a>
-<a href="../andres.html">Volver Página Principal</a>
+<a href="../andres.html">Volver</a>
 </nav>
 `;
